dfd8817b92c0d189c208a66faa703ebd114e7925
wujingtao <601595686@qq.com>
2017-08-26 01:45:32 +0800

    同步

--- start_debugging.sh
+++ start_debugging.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 docker rm mbc 
 
-#docker run -it --name mbc -p 192.168.232.190:3306:3306 -p 9229:9229 -v $(pwd)/bin:/app/bin mysql-broadcast 
-docker run -it --name mbc -p 192.168.232.190:3306:3306 -p 9229:9229 -v $(pwd)/bin:/app/bin --entrypoint /bin/bash mysql-broadcast 
+#docker run -it --name mbc -p 0.0.0.0:3306:3306 -p 9229:9229 -v $(pwd)/bin:/app/bin mysql-broadcast 
+ docker run -it --name mbc -p 0.0.0.0:3306:3306 -p 9229:9229 -v $(pwd)/bin:/app/bin --entrypoint /bin/bash mysql-broadcast 
